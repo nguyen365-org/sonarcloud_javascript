4f3ee0eacfe96c2a26b8e7e646142ba9cb205f5e
Minh Tuyen Nguyen <minh@nguyen365.com>
2025-03-02 17:58:07 -0500

    add code and tests, not using sonar-project.properties file.

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,12 @@
-const { add, subtract } = require('../src/index');
+const { add, subtract, multiply } = require('../src/index');
 test('add 1 + 2 to equal 3', () => {
     expect(add(1, 2)).toBe(3);
 });
 
 test('add 3 - 2 to equal 1', () => {
     expect(subtract(3, 2)).toBe(1);
+});
+
+test('multiply 3 with 2 to equal 6', () => {
+    expect(multiply(3, 2)).toBe(6);
 });
